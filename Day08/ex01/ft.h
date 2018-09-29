f4bf41273f482d9c6af3f3a198b05f1e405525f8
GhostWolf444 <andstefa@b1p41.academyplus.ro>
2018-09-29 15:56:07 +0300

    Day08

---
+++ Day08/ex01/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andstefa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/29 11:38:43 by andstefa          #+#    #+#             */
+/*   Updated: 2018/09/29 15:43:00 by andstefa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void		ft_putchar(char c);
+void		ft_swap(int *a, int *b);
+void		ft_putstr(char *str);
+int			ft_strlen(char *str);
+int			ft_strcmp(char *s1, char *s2);
+
+#endif
